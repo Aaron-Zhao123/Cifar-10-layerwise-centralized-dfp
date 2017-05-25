2a849f026540b9aee693e568956dce91270390ff
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-25 01:05:06 +0100

    type

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 import dfp_training
+import pickle
 def dump_to_txt_files(pt_acc_list, acc_list):
     with open("ptacc_cifar_quantize_han.txt", "w") as f:
         for item in pt_acc_list:
